2536a6835f949e87ac82bac17fb37291fe808b9a
uninstallit <67851816+uninstallit@users.noreply.github.com>
2020-09-04 23:15:03 -0400

    add more doc2vec params

--- HW1_Beqari_and_Williamson/src/doc2vec.py
+++ HW1_Beqari_and_Williamson/src/doc2vec.py
@@ -27,13 +27,15 @@ x_train["label"] = y_train
 x_eval = x_eval.to_frame()
 x_eval["label"] = y_eval
 
+error_index = 0
+
 def paragraph_to_tokens(paragraph):
     try:
         text_dict = clean_text_by_word(paragraph)
         tokens = [value.token for key, value in text_dict.items()]
         return tokens
     except TypeError:
-        print("error: nan found in paragraph - ", paragraph)
+        print('error: nan found in paragraph - {}'.format(paragraph))
     return []
 
 # step 1: convert paragraph to tokens
@@ -42,14 +44,27 @@ x_eval["tokens"]  = x_eval["comment"].apply(lambda row: paragraph_to_tokens(row)
 x_test["tokens"]  = x_test["comment"].apply(lambda row: paragraph_to_tokens(row))
     
 # *** experimental: train the vocabolary with all words ***
-all_tokens = pd.concat([x_train["tokens"], x_eval["tokens"], x_test["tokens"]])
+all_tokens = pd.concat([x_train, x_eval, x_test], keys=['tokens'], names=['tokens'])
 
 # doc2vec model
 # docs: https://radimrehurek.com/gensim/models/doc2vec.html
 cores = multiprocessing.cpu_count()
 documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(all_tokens["tokens"].tolist())]
 # documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(x_train["tokens"].tolist())]
-model_d2v = Doc2Vec(documents, vector_size=5, window=3, min_count=1, workers=cores, dbow_words=1)
+model_d2v = Doc2Vec(
+    documents, 
+    dm=0, 
+    vector_size=300, 
+    negative=5, 
+    hs=0, 
+    sample=0, 
+    window=3, 
+    min_count=2, 
+    workers=cores, 
+    alpha=0.025, 
+    min_alpha=0.001,
+    dbow_words=0, # 0-bow, 1-skip gram 
+    epochs=30)
 
 # step 2: retrieve embeddings from model
 x_train["embeddings"] = x_train["tokens"].apply(lambda row: model_d2v.infer_vector(row))
@@ -59,5 +74,3 @@ x_test["embeddings"]  = x_test["tokens"].apply(lambda row: model_d2v.infer_vecto
 x_train.to_pickle("x_train.pkl")
 x_eval.to_pickle("x_eval.pkl")
 x_test.to_pickle("x_test.pkl")
-
-print("*** done ***")
