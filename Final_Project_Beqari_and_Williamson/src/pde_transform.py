39bb0a1bdca1f084a76cab417fd810ca9f349354
uninstallit <67851816+uninstallit@users.noreply.github.com>
2020-12-10 21:41:40 -0500

    Update pde_transform.py
    fix grid plot

--- Final_Project_Beqari_and_Williamson/src/pde_transform.py
+++ Final_Project_Beqari_and_Williamson/src/pde_transform.py
@@ -1,8 +1,9 @@
 import os
 
 from tensorflow.python.keras.backend import l2_normalize
-os.environ["CUDA_VISIBLE_DEVICES"]=" "
-os.environ['TF_CPP_MIN_LOG_LEVEL']="3"
+
+os.environ["CUDA_VISIBLE_DEVICES"] = " "
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -24,104 +25,129 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
 class MonitorCallback(keras.callbacks.Callback):
-
     def on_epoch_end(self, epoch, logs=None):
 
         points = logs["points"]
 
         self.model.ax0.clear()
-        self.model.fig.suptitle("Training...")
-        surf0 = self.model.ax0.plot_surface(self.model.tt,
-                                            self.model.xx,
-                                            self.model.z,
-                                            cmap=cm.winter,
-                                            linewidth=0.1,
-                                            antialiased=False,
-                                            label='exact',
-                                            facecolor='blue',
-                                            edgecolor='blue')
-        surf0._facecolors2d=surf0._facecolors3d
-        surf0._edgecolors2d=surf0._edgecolors3d
+        # self.model.fig.suptitle("Training...")
+        surf0 = self.model.ax0.plot_surface(
+            self.model.tt,
+            self.model.xx,
+            np.reshape(self.model.z, (len(self.model.xx), len(self.model.tt))),
+            cmap=cm.winter,
+            linewidth=0.1,
+            antialiased=False,
+        )
+        # label='exact',
+        # facecolor='blue',
+        # edgecolor='blue')
+        # surf0._facecolors2d=surf0._facecolors3d
+        # surf0._edgecolors2d=surf0._edgecolors3d
+
+        u_pred = self.model.psi_predict(self.model.xt_train)
+        u_pred = np.squeeze(u_pred.numpy())
+        abs_error = np.abs(np.sum(self.model.z - u_pred))
+
         surf1 = self.model.ax0.plot_surface(
             self.model.tt,
             self.model.xx,
             np.asarray(self.model.psi_predict(self.model.xt_train)).reshape(
-                (len(self.model.x), len(self.model.t))),
+                (len(self.model.x), len(self.model.t))
+            ),
             cmap=cm.autumn,
             linewidth=0.1,
             antialiased=False,
-            label='approx.',
-            facecolor='orange',
-            edgecolor='orange')
-        surf1._facecolors2d=surf1._facecolors3d
-        surf1._edgecolors2d=surf1._edgecolors3d
-        scatter0 = self.model.ax0.scatter(points[:, 1],
-                                          points[:, 0],
-                                          -1,
-                                          s=10,
-                                          c='red',
-                                          marker="o",
-                                          label='sample')
+        )
+        #     label='approx.',
+        #     facecolor='orange',
+        #     edgecolor='orange')
+        # surf1._facecolors2d=surf1._facecolors3d
+        # surf1._edgecolors2d=surf1._edgecolors3d
+        scatter0 = self.model.ax0.scatter(
+            points[:, 1], points[:, 0], -1, s=10, c="red", marker="o", label="sample"
+        )
         # self.model.ax[0].set_zlim(-0.01, 1.01)
         self.model.ax0.zaxis.set_major_locator(LinearLocator(10))
-        self.model.ax0.zaxis.set_major_formatter(
-            FormatStrFormatter('%.02f'))
-        self.model.ax0.set_xlabel('t')
-        self.model.ax0.set_ylabel('x')
-        self.model.ax0.set_zlabel('pde solution value')
-        self.model.ax0.legend(loc='upper right')
+        self.model.ax0.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
+        self.model.ax0.set_xlabel("t")
+        self.model.ax0.set_ylabel("x")
+        self.model.ax0.set_zlabel("pde solution value")
+        self.model.ax0.legend(loc="upper right")
         plt.pause(0.01)
 
-        loss_f  = self.model.hist['loss_f']
-        loss_f.append(logs['loss_f'])
-        loss_d  = self.model.hist['loss_d']
-        loss_d.append(logs['loss_d'])
+        loss_f = self.model.hist["loss_f"]
+        loss_f.append(logs["loss_f"])
+
+        loss_d = self.model.hist["loss_d"]
+        loss_d.append(logs["loss_d"])
+
+        error = self.model.hist["error"]
+        error.append(abs_error)
 
         self.model.ax1.clear()
-        self.model.ax1.plot(loss_f)
-        self.model.ax1.plot(loss_d)
-        self.model.ax1.set_xlabel('Epoch')
-        self.model.ax1.set_ylabel('Error')
-        # self.model.axs[1].set_ylim(0.0, 0.02)
-        self.model.ax1.legend(['loss_f'], loc='upper right')
+        self.model.ax1.plot(loss_f, c="orange")
+        self.model.ax1.set_xlabel("Epoch")
+        self.model.ax1.set_ylabel("Error")
+        self.model.ax1.set_ylim(-1.0, max(loss_f[:100]) + 10)
+        self.model.ax1.legend(["loss_f"], loc="upper right")
+
+        self.model.ax2.clear()
+        self.model.ax2.plot(error, c="red")
+        self.model.ax2.set_xlabel("Epoch")
+        self.model.ax2.set_ylabel("Error")
+        self.model.ax2.set_ylim(-1.0, max(error[:100]) + 10)
+        self.model.ax2.legend(["abs. error"], loc="upper right")
+
+        self.model.ax3.clear()
+        self.model.ax3.plot(loss_d, c="green")
+        self.model.ax3.set_xlabel("Epoch")
+        self.model.ax3.set_ylabel("Error")
+        self.model.ax3.set_ylim(0.0, max(loss_d[:100]) + 0.1)
+        self.model.ax3.legend(["loss_d"], loc="upper right")
+
         plt.pause(0.01)
+
 
 class ODENetwork(tf.keras.Model):
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.05)
-    # optimizer = tf.keras.optimizers.SGD(learning_rate=0.05)
+    # optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    optimizer = tf.keras.optimizers.SGD(learning_rate=0.05)
     initializer = tf.keras.initializers.GlorotUniform()
 
     zero = tf.constant(0.0, dtype=tf.double)
-    one  = tf.constant(1.0, dtype=tf.double)
-    two  = tf.constant(2.0, dtype=tf.double)
-    pi   = tf.constant(np.pi, dtype=tf.double)
+    one = tf.constant(1.0, dtype=tf.double)
+    two = tf.constant(2.0, dtype=tf.double)
+    pi = tf.constant(np.pi, dtype=tf.double)
 
-    loss_f_trckr  = tf.keras.metrics.Mean(name="loss_f")
-    loss_d_trckr  = tf.keras.metrics.Mean(name="loss_d")
+    loss_f_trckr = tf.keras.metrics.Mean(name="loss_f")
+    loss_d_trckr = tf.keras.metrics.Mean(name="loss_d")
     hist = {
         "loss_f": [],
         "loss_d": [],
+        "error": [],
     }
 
     fig = plt.figure()
-    ax0 = fig.add_subplot(121, projection='3d')
-    ax1 = fig.add_subplot(122)
-    plt.subplots_adjust(top=0.90, hspace = 0.35)
+    ax0 = plt.subplot2grid((3, 2), (0, 0), rowspan=3, projection="3d")
+    ax1 = plt.subplot2grid((3, 2), (0, 1))
+    ax2 = plt.subplot2grid((3, 2), (1, 1))
+    ax3 = plt.subplot2grid((3, 2), (2, 1))
+    fig.tight_layout()
     plt.show(block=False)
 
-    xmin =  0.0
-    xmax =  1.0
-    tmin =  0.0
-    tmax =  1.0
+    xmin = 0.0
+    xmax = 1.0
+    tmin = 0.0
+    tmax = 1.0
 
     t = np.linspace(tmin, tmax, num=100)
     x = np.linspace(xmin, xmax, num=100)
     xx, tt = np.meshgrid(x, t)
     xt_train = np.column_stack((xx.ravel(), tt.ravel()))
 
-    z = np.sin(np.pi * xt_train[:,1]) * np.power(xt_train[:,0], 2)
-    z = np.reshape(z, (len(x), len(t)))
+    z = np.sin(np.pi * xt_train[:, 1]) * np.power(xt_train[:, 0], 2)
+    # z = np.reshape(z, (len(x), len(t)))
 
     @staticmethod
     def data(xmin, xmax, x_num, tmin, tmax, t_num):
@@ -133,7 +159,7 @@ class ODENetwork(tf.keras.Model):
 
     @staticmethod
     def exact(xt_train):
-        f_exact = np.sin(np.pi * xt_train[:,1]) * np.power(xt_train[:,0], 2)
+        f_exact = np.sin(np.pi * xt_train[:, 1]) * np.power(xt_train[:, 0], 2)
         return f_exact
 
     @tf.function
@@ -142,7 +168,9 @@ class ODENetwork(tf.keras.Model):
 
     @tf.function
     def loss_d(self, points):
-        point_1t = tf.math.multiply(points, tf.constant(np.array([0, 1]), dtype=tf.double))
+        point_1t = tf.math.multiply(
+            points, tf.constant(np.array([0, 1]), dtype=tf.double)
+        )
         point_1t = tf.math.add(point_1t, tf.constant(np.array([1, 0]), dtype=tf.double))
         point_1t = tf.expand_dims(point_1t, axis=0)
 
@@ -157,7 +185,7 @@ class ODENetwork(tf.keras.Model):
                 d2u_d1t2 = tape_ord_1.gradient(du_d1t, point_1t)
 
         d2_du1t_x2, d2_du1t_t2 = tf.unstack(d2u_d1t2, axis=1)
-        loss_d =  tf.keras.backend.abs(u_1t - d2_du1t_x2)
+        loss_d = tf.keras.backend.abs(u_1t - d2_du1t_x2)
         return loss_d
 
     @tf.function
@@ -170,20 +198,20 @@ class ODENetwork(tf.keras.Model):
             x, t = tf.unstack(points, axis=1)
 
         point_1t = tf.math.multiply(
-            points, tf.constant(np.array([0, 1]), dtype=tf.double))
-        point_1t = tf.math.add(point_1t,
-                               tf.constant(np.array([1, 0]), dtype=tf.double))
+            points, tf.constant(np.array([0, 1]), dtype=tf.double)
+        )
+        point_1t = tf.math.add(point_1t, tf.constant(np.array([1, 0]), dtype=tf.double))
 
         with tf.GradientTape(persistent=True) as tape_ord_x1:
             tape_ord_x1.watch(point_1t)
-
             u_1t = tf.cast(self(point_1t, training=False), dtype=tf.double)
+            u_1t = tf.squeeze(u_1t, axis=1)
             du_d1t = tape_ord_x1.gradient(u_1t, point_1t)
 
         du_d1t_x, du_d1t_t = tf.unstack(du_d1t, axis=1)
-        psi = (self.two * x * tf.keras.backend.sin(self.pi * t)
-               ) + (t - tf.keras.backend.pow(t, 2)) * x * (u - u_1t - du_d1t_x)
-
+        psi = (self.two * x * tf.keras.backend.sin(self.pi * t)) + (
+            t - tf.keras.backend.pow(t, 2)
+        ) * x * (u - u_1t - du_d1t_x)
         return psi
 
     @tf.function
@@ -196,11 +224,18 @@ class ODENetwork(tf.keras.Model):
         d2psi_dt2 = d2psi_dx2dt2[1]
 
         loss_f = tf.keras.backend.square(
-            d2psi_dx2 + d2psi_dt2 + psi * dpsi_dx -
-            tf.keras.backend.sin(self.pi * t) *
-            (self.two - tf.keras.backend.pow(self.pi, 2) *
-             tf.keras.backend.pow(x, 2) + self.two *
-             tf.keras.backend.pow(x, 3) * tf.keras.backend.sin(self.pi * t)))
+            d2psi_dx2
+            + d2psi_dt2
+            + psi * dpsi_dx
+            - tf.keras.backend.sin(self.pi * t)
+            * (
+                self.two
+                - tf.keras.backend.pow(self.pi, 2) * tf.keras.backend.pow(x, 2)
+                + self.two
+                * tf.keras.backend.pow(x, 3)
+                * tf.keras.backend.sin(self.pi * t)
+            )
+        )
         return loss_f
 
     def train_step(self, points):
@@ -211,8 +246,8 @@ class ODENetwork(tf.keras.Model):
 
             with tf.GradientTape(persistent=True) as tape_ord_1:
                 tape_ord_1.watch(points)
-               
-                u_training = tf.cast(self(points, training=True), dtype=tf.double)
+
+                u_training = tf.cast(self(points, training=False), dtype=tf.double)
                 psi = self.psi_func(points, u_training)
 
                 dpsi_dxdt = tape_ord_1.gradient(psi, points)
@@ -221,14 +256,17 @@ class ODENetwork(tf.keras.Model):
                 loss_f = tf.keras.backend.map_fn(
                     lambda x: self.loss_f(x[0], x[1], x[2], x[3]),
                     (points, psi, dpsi_dxdt, d2psi_dx2dt2),
-                    dtype=tf.double)
+                    dtype=tf.double,
+                )
                 loss_f = tf.reduce_mean(loss_f, axis=-1)
 
                 loss_d = tf.keras.backend.map_fn(
-                    lambda x: self.loss_d(x), (points), dtype=tf.double)
+                    lambda x: self.loss_d(x), (points), dtype=tf.double
+                )
                 loss_d = tf.reduce_mean(loss_d, axis=-1)
 
                 loss = 0.00001 * loss_f + 0.99999 * loss_d
+                # loss = loss_f
 
         self.loss_f_trckr.update_state(loss_f)
         self.loss_d_trckr.update_state(loss_d)
@@ -239,7 +277,7 @@ class ODENetwork(tf.keras.Model):
         return {
             "loss_f": self.loss_f_trckr.result(),
             "loss_d": self.loss_d_trckr.result(),
-            "points": points
+            "points": points,
         }
 
     @property
@@ -249,26 +287,26 @@ class ODENetwork(tf.keras.Model):
     def psi_predict(self, points):
         u_pred = tf.cast(self.predict(points), dtype=tf.double)
         predictions = tf.keras.backend.map_fn(
-            lambda x: self.psi_func(x[0], x[1], True), (points, u_pred),
-            dtype=tf.double)
+            lambda x: self.psi_func(x[0], x[1], True), (points, u_pred), dtype=tf.double
+        )
         return predictions
 
 
 def main():
 
-    xmin =  0.0
-    xmax =  1.0
-    tmin =  0.0
-    tmax =  1.0
+    xmin = 0.0
+    xmax = 1.0
+    tmin = 0.0
+    tmax = 1.0
 
     x, t, xx, tt, xt_train = ODENetwork.data(xmin, xmax, 100, tmin, tmax, 100)
-    f_exact                = ODENetwork.exact(xt_train)
+    f_exact = ODENetwork.exact(xt_train)
 
-    batch_size = 5 # len(xt_train)
-    epochs     = 1000 # 100000
+    batch_size = 5  # len(xt_train)
+    epochs = 10000  # 100000
 
-    inputs = keras.Input(shape=(2, ))
-    x  = layers.Dense(10, activation="tanh")(inputs)
+    inputs = keras.Input(shape=(2,))
+    x = layers.Dense(10, activation="tanh")(inputs)
     outputs = layers.Dense(1, activation="linear")(x)
     model = ODENetwork(inputs, outputs)
 
@@ -281,16 +319,16 @@ def main():
     xt_ds = xt_ds.batch(batch_size)
     xt_ds = xt_ds.cache()
 
-    model.fit(xt_ds,
-              epochs=epochs,
-              workers=-1,
-              verbose=1,
-              callbacks=[
-                  tf.keras.callbacks.TerminateOnNaN(),
-                  MonitorCallback()
-              ])
+    model.fit(
+        xt_ds,
+        epochs=epochs,
+        workers=-1,
+        verbose=1,
+        callbacks=[tf.keras.callbacks.TerminateOnNaN(), MonitorCallback()],
+    )
 
     # model.save('ode2ord_paramfit.h5')
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
